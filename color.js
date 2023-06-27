ababda319395b07b1293bba800da51dc0cdf0560
Seema-97 <134265455+Seema-97@users.noreply.github.com>
2023-06-27 18:27:58 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,13 @@
+let color= prompt("What's your favourite color")
+
+if (color=="Blue"){
+  alert("Your name is Rahul");
+}
+
+else if(color=="blue"){
+    alert("Your name is Rahul");
+}
+
+else{
+  alert("No that's not correct...stop lying");
+}
